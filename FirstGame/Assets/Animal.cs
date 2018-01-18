306f87c9b0232235cfdf2a4bdbc4ae9151a45149
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-01-18 11:15:48 -0700

    Created Animal Class and made the box meow

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	//this declares what start will do
+	void Start () {
+		print("Cats are awful.");
+		print("Not a fan of cats");
+	}
+	
+	void OnMouseDown()
+	{
+		print("Meow");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
