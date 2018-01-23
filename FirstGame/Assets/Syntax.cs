7b9ae715b26183c8d6c9d42bcbd40be47710d1e1
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-01-23 11:16:20 -0700

    Conventions and syntax

---
+++ FirstGame/Assets/Syntax.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+	
+
+
+	/*Classes are a box that has smaller boxes called fuctions that are commands
+	Functions have commands in them that use data.
+	you can call a function to bring up data from another function.
+		this is a class -> public class Comments : MonoBehaviour{
+		this is a function -> void Start(){
+			print("Hello"); <-this is a calling of a function
+		} you always have to end a code block with brackets or it is an open box
+		there can be multiple functions in a class
+		void Jump(){
+			print("I can't jump");
+		}
+		} <- like this
+	*/
+public class Syntax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
