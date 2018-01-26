0fcddf4022f81e7dfc67bfa9d655e541bed0ff53
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-01-25 20:16:41 -0700

    Finished the inclass assignment for variables

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -5,7 +5,15 @@ using UnityEngine.UI;
 
 public class Animal : MonoBehaviour {
 
-
+	//varibles are different data types that store different information
+	//there can be int which are whole numbers
+	//or float which are decimals but those take up more storage and can make the game run slower
+	//or strings hold phrases
+	public float damageDone;
+	public string characterName;
+	public int enemyHealth;
+	public int enemyType; 
+	public int characterHealth;
 	public string name;
 
 	public int foodCount;
