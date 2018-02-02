6169c3353e4e204c8f4e6767c755dabaec3d87fd
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-02-01 21:47:12 -0700

    Else clauses

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -30,27 +30,49 @@ public class Animal : MonoBehaviour {
 			//do this block of code
 			//}
 		//however if the argument is false then it will not even look into the code block
+		
+		
+		//else clauses are used to follow an if statement
+		//they are the code that happens when the if is false
+		//You do not always need an else clause but it could come in handy
+		//if you have more than a true or false that you are 
+		//checking it is not a wise choice to go with an if statement in the first place
 		if(animalName == "Cat"){
 			myUIText.text = "Meow";
+		}
+		else{
+			myUIText.text = "Wrong";
 		}
 		if(foodCount <= 0){
 			myUIText.text = "The " + name + " needs food.";
 		}
+		else{
+			print("How dare you think you are out of food");
+		}
 
 		if(food == true){
 			myUIText.text = "Pet has food.";
+		}else
+		{
+			print("The pet is starving");
 		}
 		if(1+2 == 3){
 			print("3 is a magic number");
 		}
+		
 		if(foodCount > 0){
 			myUIText.text = "the pet still has food";
+		}else
+		{
+			print("The pet is dead");
 		}
 		if(4+1 == 5){
 			print("you're a dummy.");
 		}
 		if(animalName == "Dog"){
 			print("bark");
+		}else{
+			print("dogs suck");
 		}
 		if(animalName == "fish"){
 			print("blub blub");
