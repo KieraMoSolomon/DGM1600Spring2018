482a31710a251aff734f43a8e5ceb3564fa71c3c
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-02-08 10:21:54 -0700

    Started math operators

---
+++ FirstGame/Assets/MathOperators.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour {
+
+	public int A;
+	public int B;
+	public int C;
+
+	// Use this for initialization
+	void Start () {
+
+		if(A + B == C){
+			print(C);
+		}	
+		if(C - A == B){
+			print(B);
+		}
+		if(A * B == C){
+			print(C);
+		}
+		if(A / B == C){
+			print(C);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
