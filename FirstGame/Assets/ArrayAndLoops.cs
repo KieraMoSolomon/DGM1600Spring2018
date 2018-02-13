97802b0204d248a2ba09e8fed7b441af23374392
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-02-13 10:48:36 -0700

    created an array and loop c# file

---
+++ FirstGame/Assets/ArrayAndLoops.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayAndLoops : MonoBehaviour {
+
+
+	public string[] Players;
+	public int[] HighScores;
+	public PowerUp[] PowerUp;
+
+	// Use this for initialization
+	void Start () {
+		foreach(var player in Players){
+			//do something
+			print(player);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
