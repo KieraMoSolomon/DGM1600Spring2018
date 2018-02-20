d11b695dfd9a580a713264654db8519755bd252a
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-02-20 11:17:25 -0700

    Worked on lists

---
+++ FirstGame/Assets/ListAndLoops.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListAndLoops : MonoBehaviour {
+
+	public TheIncredibles TheIncredibles;
+	public Text AddMember;
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown()
+	{
+		TheIncredibles.FamilyList.Add(AddMember.text);
+	}
+}
