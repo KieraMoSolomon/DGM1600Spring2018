3de8ff1ded4a46010a80661192588d434674b0cd
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-01 10:35:34 -0700

    Start of Functions with parameters

---
+++ FirstGame/Assets/FunctionsWithParam.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionsWithParam : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		print("Hit something");
+	}
+}
