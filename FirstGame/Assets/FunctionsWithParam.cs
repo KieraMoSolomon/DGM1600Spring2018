b40ce57b99ee047b7f9b65019c0055b88faec4fc
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-01 11:19:12 -0700

    Worked on how to do power ups better

--- FirstGame/Assets/FunctionsWithParam.cs
+++ FirstGame/Assets/FunctionsWithParam.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class FunctionsWithParam : MonoBehaviour {
 
+
+	public int Health = 20;
+
 	void OnTriggerEnter(Collider other) {
 		print("Hit something");
+		print(other);
+
+		AddNumbers(14, 2);
+		PlayerData("Bob");
+		AddHealth(25);
 	}
+
+	void AddHealth(int newHealth){
+		Health += newHealth;
+	}
+
+	void AddNumbers(int a, int b){
+		print(a += b);
+	}
+
+	void PlayerData(string name){
+		print("This player is " + name);
+	}
+
+
 }
---
+++ FirstGame/Assets/Powerup/PowerUpTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpTrigger : MonoBehaviour {
+
+	public PowerUpBase myPowerUp;
+	public Player myPlayer;
+
+	void OnTriggerEnter() {
+		AddPower(myPowerUp);
+	}
+
+	void AddPower(PowerUpBase powerUp){
+		//Do work
+		//print(powerUp.powerLevel);
+		myPlayer.Health += myPowerUp.powerLevel;
+	}
+
+}
