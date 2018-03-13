6a75244df1a16151ab4187774da866ffdeda908e
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-13 10:15:36 -0600

    Calculator

---
+++ FirstGame/Assets/Calculator/Calculation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Calculation : ScriptableObject {
+	public int Calculate(string a, string b){
+		int anum = int.Parse(a);
+		int bnum = int.Parse(b);
+
+		return anum + bnum;
+	}
+	
+}
