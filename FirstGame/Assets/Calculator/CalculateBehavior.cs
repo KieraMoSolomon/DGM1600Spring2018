6e287248fefc0e471da1f82d9017eb375669add7
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-18 15:58:09 -0600

    Got the buttons to disable

--- FirstGame/Assets/Calculator/CalculateBehavior.cs
+++ FirstGame/Assets/Calculator/CalculateBehavior.cs
@@ -9,8 +9,28 @@ public class CalculateBehavior : MonoBehaviour {
 	public InputField Input1;
 	public InputField Input2;
 	public Text Solution;
+	public Button Addition;
+	public Button Subtraction;
+	public Button Multiply;
+	public Button Divide;
 
 	public void RunCalculation(){
-		Solution.text = Calculation.Calculate(Input1.text, Input2.text).ToString();
+		Solution.text = Calculation.Calculate(Input1.text, Input2.text);
+	}
+
+	public void DisableAddButtonOnClick(){
+		Addition.interactable = false;
+	}
+
+	public void DisableSubButton(){
+		Subtraction.interactable = false;
+	}
+
+	public void DisableMultButton(){
+		Multiply.interactable = false;
+	}
+
+	public void DisableDivButton(){
+		Divide.interactable = false;
 	}
 }
