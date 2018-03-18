e86cffe92da8c783cc293b1e6aea52c0489f03e2
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-18 15:59:42 -0600

    Got the falling number to show up on a text box

---
+++ FirstGame/Assets/Calculator/FallingNum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FallingNum : MonoBehaviour {
+	public float fallingNumber;
+	public Text fallNum;
+
+	// Use this for initialization
+	void Start () {
+		CreateFallingNum();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CreateFallingNum(){
+		fallingNumber = Random.Range(0, 1000);
+		fallNum.text = fallingNumber.ToString();
+	}
+}
