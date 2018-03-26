2f97a284e64d1481f4ab1beefdc0c5864f9368e6
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-26 14:00:41 -0600

    Math Operators and Logic Operators comments still needs examples

--- FirstGame/Assets/MathOperators.cs
+++ FirstGame/Assets/MathOperators.cs
@@ -10,14 +10,33 @@ public class MathOperators : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		/*math operators are the same as the ones we have been learning
+		about since we were in first grade. The one that is new for scripting
+		is the % which gives the remainder of the divide. It is a great way to
+		check if a number is odd or even.*/
+		/*This is also going to be my logical operators assignment.
+		Logical operators are to compare data with other data
+		they are useful to use with if statements and different things
+		that need arguments to check.
+		 */
 		if(A <= 0){
 			A++;
 		}
 		if(A >= 10){
 			A--;
 		}
-		
+		if(A > B){
+			A = A + B;
+			B = A + C;
+			C = C - B;
+		}
+		if(C == 10){
+			C = 5;
+		}
+		if(){
+			
+		}
+
 
 	}
 	
