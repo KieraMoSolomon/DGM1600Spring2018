51dd7ea1780301f6ad78be4beb7d8af93869fba0
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-03-29 18:04:51 -0600

    Using Scriptable objects with character controllers

--- FirstGame/Assets/CharacterController/CharacterControllerLearn.cs
+++ FirstGame/Assets/CharacterController/CharacterControllerLearn.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class CharacterControllerLearn : MonoBehaviour {
 
-	public CCScriptable Script;
+    public MovePattern MovePattern;
 	private CharacterController controller;
-    private Vector3 moveDirection = Vector3.zero;
+    
 
 	void Start(){
 		controller = GetComponent<CharacterController>();
 	}
 
     void Update() {
-        if (controller.isGrounded) {
-            moveDirection.x = Input.GetAxis("Horizontal");
-			moveDirection.z = Input.GetAxis("Vertical");
-			moveDirection.y = 0;
-
-			moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= Script.speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = Script.jumpSpeed;
-            
-        }
-        moveDirection.y -= Script.gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+       MovePattern.Move(controller, transform);
 	}
 	
 }
