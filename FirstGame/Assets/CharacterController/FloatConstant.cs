391fbc68dd4eaff23b565d409bad0f9d8cbc4d98
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-04-03 11:14:19 -0600

    Made the inputs into scriptable objects

---
+++ FirstGame/Assets/CharacterController/FloatConstant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatConstant : InputBase {
+
+	public float floatValue = 0;
+
+	public override float SetFloat(){
+		return floatValue;
+	}
+}
---
+++ FirstGame/Assets/CharacterController/FloatInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewInput")]
+public class FloatInput : InputBase {
+
+	public string inputName = "Horizontal";
+
+	public override float SetFloat(){
+		return Input.GetAxis(inputName);
+	}
+}
