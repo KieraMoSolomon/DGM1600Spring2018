f11726e0eda05f021b465b2ef9b28cc3be717d0a
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-04-04 13:29:21 -0600

    Did functions with parameters and void functions in this file

--- FirstGame/Assets/FunctionsWithParam.cs
+++ FirstGame/Assets/FunctionsWithParam.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class FunctionsWithParam : MonoBehaviour {
 
-
+/*This is also for void functions! Void functions don't return anything.
+they can take in parameters but they will not send data. They just return a
+null value. */
+/*Functions with parameters need variables from other functions
+they use those variables to complete their code block. When you call a function
+that requires a parameter it will demand that there is the correct type of variable sent
+to the function you are calling.
+ */
 	public int Health = 20;
 
 	void OnTriggerEnter(Collider other) {
@@ -27,6 +34,23 @@ public class FunctionsWithParam : MonoBehaviour {
 	void PlayerData(string name){
 		print("This player is " + name);
 	}
-
+	void SubNumbers(int a, int b){
+		print(a - b);
+	}
+	void MultNumbers(int a, int b){
+		print(a * b);
+	}
+	void DivNumbers(float a, float b){
+		print(a/b);
+	}
+	void PlayerHealing(int healingPower){
+		Health += healingPower;
+	}
+	void PlayerHelper(int identity){
+		print(identity);
+	}
+	void DontKnow(string yep){
+		print(yep);
+	}
 
 }
