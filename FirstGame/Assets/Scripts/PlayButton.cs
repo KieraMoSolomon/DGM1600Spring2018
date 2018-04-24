c23a46e1be7c26fdb303e59a3c88c4f714a6f965
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-04-24 13:26:40 -0600

    Got the pause menu to work and switch back to the start menu

--- FirstGame/Assets/Scripts/PlayButton.cs
+++ FirstGame/Assets/Scripts/PlayButton.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 public class PlayButton : MonoBehaviour {
 
 	public Transform startButton;
+	public Transform pausePanel;
+
 	//public gameObject menu;
 	public Animator animator;
 
-	public void WhenClicked(){
+	public void StartButtonWhenClicked(){
 		animator.SetTrigger("StartGame");
 		startButton.gameObject.SetActive(false);
+		pausePanel.gameObject.SetActive(true);
+	}
+
+	public void PauseButtonOnClick(){
+		animator.SetTrigger("PauseGame");
+		startButton.gameObject.SetActive(true);
+		pausePanel.gameObject.SetActive(false);
 	}
 }
