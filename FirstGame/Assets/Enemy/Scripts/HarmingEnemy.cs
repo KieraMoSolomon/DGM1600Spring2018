fa81aefb99b4121e00e580072b0111e84109c424
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-05-03 22:15:59 -0600

    Got a while loop put in, but wanted to save before trying it

--- FirstGame/Assets/Enemy/Scripts/HarmingEnemy.cs
+++ FirstGame/Assets/Enemy/Scripts/HarmingEnemy.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class HarmingEnemy : MonoBehaviour {
 
+	public int enemyHealth = 50;
+	public int currentHealth;
+	ParticleSystem hitPart;
+	bool enemyDead;
+
 	// Use this for initialization
 	void Start () {
-		
+		currentHealth = enemyHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(enemyDead){
+			Destroy(gameObject);
+		}
 		
+	}
+
+	void TakesDamage(int amount, Vector3 placeHit){
+		if(enemyDead){
+			return;
+		}
+		
+		currentHealth -= amount;
+		hitPart.transform.position = placeHit;
+		hitPart.Play();
+		if(currentHealth <= 0){
+			enemyDead = true;
+		}
 	}
 }
---
+++ FirstGame/Assets/Players/Fighting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighting : MonoBehaviour {
+
+	public int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(Input.GetButton("Fire1")){
+			Shoot();
+		}
+	}
+
+	void Shoot(){
+		//instantiate bullet
+
+		if(/*Bullet hit something*/true){
+			//HarmingEnemy harm = /*bullet hit. collider.GetComponent();*/
+			
+			if(/*harm != null*/true){
+				//harm.TakeDamage(damage, /*vector3 of place hit*/);
+			}
+			//destroy bullet
+		}
+	}
+}
