180cf456bf68b46b7baada27701ec2b97a5c1c05
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-05-04 15:03:56 -0600

    Trying more ways to make it better

--- FirstGame/Assets/Players/Fighting.cs
+++ FirstGame/Assets/Players/Fighting.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Fighting : MonoBehaviour {
 
 	public int damage = 10;
+	public float timeBetween = 0.2f;
+	public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+	float timer;
 
 	// Use this for initialization
 	void Start () {
@@ -13,21 +18,28 @@ public class Fighting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		while(Input.GetButton("Fire1")){
-			Shoot();
+		timer += Time.deltaTime;
+		if(timer >= timeBetween){
+			while(Input.GetButton("Fire1")){
+				Shoot();
+			}
 		}
 	}
 
 	void Shoot(){
+		timer = 0f;
 		//instantiate bullet
-
+		//var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		//bullet.GetComponent().velocity = bullet.transform.forward * 5;
 		if(/*Bullet hit something*/true){
 			//HarmingEnemy harm = /*bullet hit. collider.GetComponent();*/
 			
 			if(/*harm != null*/true){
+				//do a get function to get the placehit variable from the bullet script
 				//harm.TakeDamage(damage, /*vector3 of place hit*/);
 			}
 			//destroy bullet
+			//Destroy(bullet);
 		}
 	}
 }
